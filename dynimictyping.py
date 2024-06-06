c0567244c13c24c1fee8b67949c9a6fb8afe3de4
rajukaji <rajulamarainon@gmail.com>
2024-06-07 01:56:38 +0545

    variables

---
+++ dynimictyping.py
@@ -0,0 +1,6 @@
+count = 1000
+print(type(count))
+
+print(type(1.1))
+print (type(True))
+print(type("hello"))
---
+++ variables.py
@@ -0,0 +1,10 @@
+students_count = 1000
+rating = 4.09
+isTrue = True
+multiLines = """
+multiple
+lines
+"""
+
+x, y = 1, 2
+x = y = 1
