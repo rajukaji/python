1d51d3f4259b83cfa8f9d6314cc6f227acc6855a
rajukaji <rajulamarainon@gmail.com>
2024-06-09 01:01:47 +0545

    conditional statement

---
+++ conditionalstatement.py
@@ -0,0 +1,23 @@
+age = 22
+
+if age >= 18 :
+    print("can vote!")
+    print("can vote!")
+    print("can vote!")
+#use indentation in python
+elif age >= 13 :
+    print("Cannot unless over 18!")
+    
+elif age < 13 :
+    print("just a child")
+    
+else :
+    print("can vote indeed if the are older ")
+    
+
+#use pass keyword to have an empty block
+x = -1
+if x > 1:
+    pass
+else:
+    pass
