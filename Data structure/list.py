2a85fac5aa4dc5800435610df4aed3d24257d5c7
rajukaji <rajulamarainon@gmail.com>
2024-06-14 19:36:27 +0545

    list data structure in python

---
+++ Data structure/list.py
@@ -0,0 +1,24 @@
+#lists
+vowels = ['a', 'e', 'i', 'o', 'u']
+
+matric = [[0, 1], [2, 3]]
+
+#list of 100 zeros
+
+zeros = [0] * 100
+
+print(zeros)
+
+combined = zeros + vowels
+
+print(combined)
+
+#list () and range() functions
+
+print(list (range(20)))
+
+print(list (range(1, 15)))
+
+print(list("hello world"))#each characters is an item of the list now. 
+
+print(len(list("Raju")))#list length
