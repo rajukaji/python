2a3e1c349a06c9705ba7fe917b62d52679908ec8
rajukaji <rajulamarainon@gmail.com>
2024-06-16 15:48:10 +0545

    accessing list

---
+++ Data structure/accessingitems.py
@@ -0,0 +1,29 @@
+#accessging individual elements
+letter = ['a', 'b', 'c', 'd']
+
+print(letter[0])#1st item
+
+print(letter[-1])#last item
+
+print(letter) #entire list
+
+print(letter[:]) #original list same
+
+#to slice a string
+print(letter[0:2])#first 2 \items
+
+print(letter[:2])#first 2 \items
+
+print(letter[0:])#first 2 \items
+
+print(letter[::2]) #every 2nd element
+
+numbers = list(range(20))
+
+print(numbers)
+
+print(numbers[::2])
+
+print(numbers[::-1])#in reverse order
+
+
