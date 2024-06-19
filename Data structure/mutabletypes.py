84bf864368419b05593a202a08c221ea98b18d77
rajukaji <rajulamarainon@gmail.com>
2024-06-20 00:36:47 +0545

    mutable types, means, value can be changd for mutable types

---
+++ Data structure/mutabletypes.py
@@ -0,0 +1,19 @@
+#there are mutable types like list, dictionary, and sets
+
+#list 
+lis = ['a', 'b', 'c']
+
+#dictionary
+dict = {"a" : 97, "A" : 65}
+
+#sets
+sets = {'a', 'e', 'i', 'o', 'u'}
+
+#each fields are changeable here
+
+#but the immutable types like, bool, string, etc cannnot be changed
+#str = "hello world" , here the elements of this string cannot be changed
+
+print(lis)
+print(dict)
+print(sets)
