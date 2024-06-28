1fed7ef7fb9706c0edbc9791696ee556abb416ee
rajukaji <rajulamarainon@gmail.com>
2024-06-29 02:16:35 +0545

    structure

--- Data structure/generatorobj.py
+++ Data structure/generatorobj.py
@@ -4,3 +4,20 @@ values = [x * 2 for x in range(10)]#using 10000000000 instead 10 is not possible
 
 for x in values:
     print(x)
+
+
+#using a generator objects, they are iterable just like list
+#they dont store all the values in the memory
+
+from sys import getsizeof
+
+values = (x * 2 for x in range(10))
+print(values)
+
+for x in values:
+    print(x)
+
+
+values = (x * 2 for x in range(100000))
+print("gen:", getsizeof(values))
+#generator doesn't produce the length of how many items it produce
