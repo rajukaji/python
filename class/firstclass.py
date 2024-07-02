f754f3c0f7896e5111ba4d2398528b25e8e33c31
rajukaji <rajulamarainon@gmail.com>
2024-07-02 09:31:17 +0545

    Classes in python

---
+++ class/firstclass.py
@@ -0,0 +1,17 @@
+#class: blueprint for creating new objects
+#object: is the instance of class
+
+#class:Human
+#objects: John, Ramu, gobarePetu
+#class has methods
+
+#creating class
+
+class ClassName:
+    def func(self):
+        print("self")
+
+function = ClassName()
+print(type(function))
+
+print(isinstance(function, ClassName))
