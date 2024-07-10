47fb19f2a2c480d5a9ace3388aa3339c11b9fa2a
rajukaji <rajulamarainon@gmail.com>
2024-07-11 00:30:22 +0545

    sum function python

---
+++ misc/functionADD.py
@@ -0,0 +1,13 @@
+# Replace ___ with your code
+
+# define the function add_three_numbers()
+def add_three_numbers(n1, n2, n3):
+    print("Sum :: ", n1 + n2 + n3)
+
+# take input for num1, num2, num3
+num1 = int(input("Print 1st number :: "))
+num2 = int(input("Print 2nd number :: "))
+num3 = int(input("Print 3rd number :: "))
+
+# call the function with num1, num2 and num3
+add_three_numbers(num1, num2, num3)
