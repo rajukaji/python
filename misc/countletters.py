5f4f48e1f17455400a17fd1259818402c82aebdf
rajukaji <rajulamarainon@gmail.com>
2024-07-19 00:43:48 +0545

    python

---
+++ misc/countletters.py
@@ -0,0 +1,5 @@
+#ord(char type) to see the ascii value of a character
+
+print(ord('a'))
+
+char = 'Hello'
---
+++ misc/data structure/listtostring.py
@@ -0,0 +1,7 @@
+list = ['a', 'bird', 'racoon', 'dood']
+
+# ' '.join(pass list) method
+# ' '. is a word separator
+
+print(' '.join(list)) # is now a string
+
