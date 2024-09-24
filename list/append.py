0883ca294e564f2a6dc47097158db91754e87673
rajukaji <rajulamarainon@gmail.com>
2024-09-25 01:21:16 +0545

    append and extend

---
+++ list/append.py
@@ -0,0 +1,7 @@
+lst = [1, 2, 3, 4]
+
+lst.append(5)
+print(lst)
+
+lst.append([1, 2, 3])
+print(lst)
---
+++ list/excend.py
@@ -0,0 +1,8 @@
+lst = [1, 2, 3, 4, 5]
+lst1 = [6, 7, 8, 9]
+
+lst.extend(lst1)
+#extending the list
+print(lst)
+
+help(lst)
