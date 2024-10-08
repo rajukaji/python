cc07ff8034115c681aa0a027d34c889ff7059941
rajukaji <rajulamarainon@gmail.com>
2024-10-09 02:57:13 +0545

    linked list append

--- dsa/Linked List/Inserting deleting/README.md
+++ dsa/Linked List/Inserting deleting/README.md
@@ -3,3 +3,17 @@ In this lesson, we will learn about the following operations in a linked list:
 
 Inserting into a linked list
 Deleting from a linked list
+
+Insert Into a Linked List
+We can insert a node at any position in a linked list. In general, there are three cases for node insertion.
+
+Insert a node at the end of a linked list (append).
+Insert at the beginning of a linked list.
+Insert at any given position.
+Let's see how we can append a node to a linked list.
+
+Time Complexity
+To append an element to the end of a linked list, we first need to traverse through the list to reach the end. Hence, this results in a linear time complexity.
+
+Time Complexity: O(n).
+
---
+++ dsa/Linked List/Inserting deleting/linkedlistappend.py
@@ -0,0 +1,55 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    # create the linked list: 90->80->60->50
+    def create_linked_list(self):
+        node1 = Node(90)
+        node2 = Node(80)
+        node3 = Node(60)
+        node4 = Node(50)
+
+        self.head = node1
+        node1.next = node2
+        node2.next = node3
+        node3.next = node4
+
+    # display linked list in format: A->B->C
+    def display(self):
+        current = self.head
+        while current:
+            print(f"{current.data}", end="->")
+            current = current.next
+        print(None)
+
+    # method to append a node
+    def append(self, data):
+
+        new_node = Node(data)
+
+        if not self.head:
+            self.head = new_node
+            return
+
+        current = self.head
+
+        while current.next:
+            current = current.next
+        current.next = new_node
+
+linked_list = LinkedList()
+
+# create the initial linked list
+linked_list.create_linked_list()
+
+# input for data to append
+data = int(input())
+linked_list.append(data)
+
+# print the updated linked list
+linked_list.display()
