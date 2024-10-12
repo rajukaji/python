8dfbf4ee7063fcf40c282dcb8ee33d2e9ffcbec2
rajukaji <rajulamarainon@gmail.com>
2024-10-12 10:28:24 +0545

    insert modified

--- problems/insert.py
+++ problems/insert.py
@@ -4,3 +4,13 @@ def insert_element(lst,index,value):
 
 print(insert_element([1, 2, 3, 5], 3, 4))
 #insert into a list, with index and value, insert(index, value)
+
+
+
+lst = ['a', 'b', 'd', 'e']
+print(lst)
+
+lst.insert(2, 'c')
+# insert into  the second item
+
+print(lst)
