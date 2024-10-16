f0f6acc2427921fa50014f144c47bc616d54252d
rajukaji <rajulamarainon@gmail.com>
2024-10-17 01:00:40 +0545

    csv extract

---
+++ csv file handling/extract/extract.py
@@ -0,0 +1,16 @@
+# CODE FROM THE PREVIOUS SCREEN
+opened_file = open('AppleStore.csv')
+from csv import reader
+read_file = reader(opened_file)
+apps_data = list(read_file)
+
+def extract(index):
+    column = []    
+    for row in apps_data[1:]:
+        value = row[index]
+        column.append(value)    
+    return column
+
+genres = extract(11)
+
+# to extract a column that we need with index
