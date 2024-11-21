90ea1042f4080398f75de08afb9d62e596873025
rajukaji <rajulamarainon@gmail.com>
2024-11-22 00:32:27 +0545

    print initials

---
+++ projects/capstone/print_initials.py
@@ -0,0 +1,18 @@
+# R
+# L
+print('-' * 20)
+
+print('R R R R')
+print('R       R')
+print('R       R')
+print('R R R R')
+print('R   R')
+print('R     R')
+print('R       R')
+
+print('\n\n')
+
+print('L\n'*5 + 'L')
+print('L ' * 5)
+
+print('='*20)
