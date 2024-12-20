3739a0bbd9b5be71a0ebbb534c927d5f7945cf5e
rajukaji <rajulamarainon@gmail.com>
2024-12-20 21:10:38 +0545

    regex expression

---
+++ pandas/datacleaning/regex expression basics/script.py
@@ -0,0 +1,11 @@
+# Regular Expression Basics
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import re
+
+# data set 
+# https://www.kaggle.com/datasets/hacker-news/hacker-news-posts
+
+hn = pd.read_csv('hacker_news.csv')
