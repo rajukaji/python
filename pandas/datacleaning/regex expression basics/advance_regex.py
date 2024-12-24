1d2c1eaa964ca01321c3ec3cf9d8979ba453e671
rajukaji <rajulamarainon@gmail.com>
2024-12-25 00:29:00 +0545

    pandas

--- pandas/datacleaning/regex expression basics/advance_regex.py
+++ pandas/datacleaning/regex expression basics/advance_regex.py
@@ -181,3 +181,319 @@ first_ten = first_10_matches(pattern)
 
 #  Using Lookarounds to Control Matches Based on Surrounding Text
 
+'''
+Let's look at the result of the previous exercise:
+
+365                                             The new C standards are worth it
+444                                  Moz raises $10m Series C from Foundry Group
+521                                 Fuchsia: Micro kernel written in C by Google
+1307                                   Show HN: Yupp, yet another C preprocessor
+1326                                            The C standard formalized in Coq
+1365                                                 GNU C Library 2.23 released
+1429    Cysignals: signal handling (SIGINT, SIGSEGV, ) for calling C from Python
+1620                                               SDCC  Small Device C Compiler
+1949         Rewriting a Ruby C Extension in Rust: How a Naive One-Liner Beats C
+2195                    MyHTML  HTML Parser on Pure C with POSIX Threads Support
+Name: title, dtype: object
+
+Explain
+
+Copy
+It looks like we're getting close. In our first 10 matches we have one irrelevant result,
+ which is about "Series C," a term used to represent a particular type of startup fundraising.
+
+Additionally, we've run into the same issue as we did in the previous lesson — by using a negative set,
+ we may have eliminated any instances where the last character of the title is "C" (the second last line 
+ of output matches in spite of the fact that it ends with "C," because it also has "C" earlier in the string).
+
+Neither of these can be avoided using negative sets, which are used to allow multiple matches for a 
+single character. Instead we'll need a new tool: lookarounds.
+
+Lookarounds let us define a character or sequence of characters that either must or must not come 
+before or after our regex match. There are four types of lookarounds:
+
+
+'''
+
+'''
+lookaround              Pattern         Explanation
+
+positive lookahead      zzz(?=abc)      matches zzz only when it is followed by abc
+
+negative lookahead      zzz(?!abc)      matches zzz only when it is not followed by abc
+
+positive lookbehind     (?<=abc)zzz     matches zzz only when it is preceded by abc
+
+negative lookbehind     (?<!abc)zzz     matches zzz only when it is not preceded by abc
+'''
+
+'''
+These tips can help you remember the syntax for lookarounds:
+
+Inside the parentheses, the first character of a lookaround is always ?.
+If the lookaround is a lookbehind, the next character will be <, 
+which you can think of as an arrow head pointing behind the match.
+The next character indicates whether the lookaround is positive (=) or negative (!).
+While building regular expressions, you can also use the OR operator |.
+ The | operator allows the expression to match either the condition preceding it or the one following it. 
+ For example, the expression ((?![+.])|\.$) matches either instances that are not
+   followed by . or + or instances where the match is at the end of the sentence.
+     This becomes handy in complex lookaround operations.
+
+Let's create some test data that we'll use to illustrate how lookarounds work:
+
+test_cases = ['Red_Green_Blue',
+              'Yellow_Green_Red',
+              'Red_Green_Red',
+              'Yellow_Green_Blue',
+              'Green']
+
+We'll also create a function that will loop over our test cases and tell us whether our pattern matches.
+ We'll use the re module rather than pandas since it tells us the exact text that matches,
+   which will help us understand how the lookaround is working:
+
+def run_test_cases(pattern):
+    for tc in test_cases:
+        result = re.search(pattern, tc)
+        print(result or "NO MATCH")
+
+
+In each instance, we'll aim to match the substring Green depending on the characters that precede or
+ follow it. Let's start by using a positive lookahead to include instances where the match is followed 
+ by the substring _Blue. We'll include the underscore character in the lookahead, otherwise
+   we will get zero matches:
+
+run_test_cases(r"Green(?=_Blue)")
+
+
+_sre.SRE_Match object; span=(4, 9), match='Green'
+NO MATCH
+NO MATCH
+_sre.SRE_Match object; span=(7, 12), match='Green'
+NO MATCH
+
+Notice how the matches themselves are purely the text Green and don't include the lookahead. 
+Let's look at a negative lookahead to include instances where the match is not followed by the substring _Red:
+
+run_test_cases(r"Green(?!_Red)")
+
+
+_sre.SRE_Match object; span=(4, 9), match='Green'
+NO MATCH
+NO MATCH
+_sre.SRE_Match object; span=(7, 12), match='Green'
+_sre.SRE_Match object; span=(0, 5), match='Green'
+
+
+Next we'll use a positive lookbehind to include instances where the match is preceded by the substring Red_:
+
+run_test_cases(r"(?<=Red_)Green")
+
+_sre.SRE_Match object; span=(4, 9), match='Green'
+NO MATCH
+_sre.SRE_Match object; span=(4, 9), match='Green'
+NO MATCH
+NO MATCH
+
+
+And finally, using a negative lookbehind to include instances where the match isn't preceded
+ by the substring Yellow_:
+
+run_test_cases(r"(?
+
+
+_sre.SRE_Match object; span=(4, 9), match='Green'
+NO MATCH
+_sre.SRE_Match object; span=(4, 9), match='Green'
+NO MATCH
+_sre.SRE_Match object; span=(0, 5), match='Green'
+
+
+The contents of a lookaround can include any other regular expression component. For instance, 
+here is an example where we match only cases that are followed by exactly five characters:
+
+run_test_cases(r"Green(?=.{5})")
+
+
+_sre.SRE_Match object; span=(4, 9), match='Green'
+NO MATCH
+NO MATCH
+_sre.SRE_Match object; span=(7, 12), match='Green'
+NO MATCH
+
+
+The second and third test cases are followed by four characters, not five, and the last test case
+ isn't followed by anything.
+
+Sometimes programming languages won't implement support for all lookarounds (notably, 
+lookbehinds are not in the official JavaScript specification). As an example, to get full support 
+in the RegExr tool, you'll need to set it to use the PCRE regex engine.
+
+In this exercise, we're going to use lookarounds to refine the regular expression we built on 
+the last screen to capture mentions of the "C" programming language. As a reminder, here is the
+last of the regular expressions we attempted to use with this exercise earlier, and the resultant 
+titles that match:
+
+first_10_matches(r"\b[Cc]\b[^.+]")
+
+
+364                                             The new C standards are worth it
+444                                  Moz raises $10m Series C from Foundry Group
+521                                 Fuchsia: Micro kernel written in C by Google
+1308                                   Show HN: Yupp, yet another C preprocessor
+1327                                            The C standard formalized in Coq
+1366                                                 GNU C Library 2.23 released
+1430    Cysignals: signal handling (SIGINT, SIGSEGV, ) for calling C from Python
+1621                                               SDCC  Small Device C Compiler
+1950         Rewriting a Ruby C Extension in Rust: How a Naive One-Liner Beats C
+2196                    MyHTML  HTML Parser on Pure C with POSIX Threads Support
+Name: title, dtype: object
+
+
+Let's now use lookarounds to exclude the matches we don't want. We want to:
+
+Keep excluding matches that are followed by . or +, but still match cases where "C" 
+falls at the end of the sentence.
+Exclude matches that have the word 'Series' immediately preceding them.
+'''
+
+'''
+Instructions
+Write a regular expression and assign it to pattern. The regular expression should:
+Match instances of C or c where they are not preceded or followed by another word character.
+From the match above:
+Exclude instances where it is followed by a . or + character, without removing instances where the match occurs at the end of the sentence. The end anchor (\.$) will help to preserve occurrences of c or C at the end of the sentence.
+Exclude instances where the word 'Series' immediately precedes the match.
+'''
+# pattern = r'(?<!Series\s)\b[Cc]\b((?![ +.])|\.$)'
+
+
+# c_mentions = titles.str.contains(pattern).sum()
+
+pattern = r"(?<!Series\s)\b[Cc]\b((?![+.])|\.$)" 
+# | or operator, we are using it to match the end of the sentence
+# we are using negative lookbehind to exclude the word Series
+# we are using negative lookahead to exclude the characters + and . after C
+# we are using word boundary to match C or c
+# \s is a whitespace character
+# $ is an end of the sentence
+
+c_mentions = titles.str.contains(pattern).sum()
+
+
+# BackReferences: Using Capture Groups in a RegEx Pattern
+
+
+'''
+Let's say we wanted to identify strings that had words with double letters, like the "ee" in "feed."
+ Because we don't know ahead of time what letters might be repeated, we need a way to specify a 
+ capture group and then to repeat it. We can do this with backreferences.
+
+Whenever we have one or more capture groups, we can refer to them using integers left to right as shown 
+in this regex that matches the string HelloGoodbye:
+'''
+
+'''
+# (hello)(goodbye)
+# hello in one groupd and goodbye in another
+
+
+# (hello)(goodbye)\2\1
+
+# 2 is the contents of group 2,
+# 2 is the contents of group 1. 
+
+# The regular expression above will match the text HelloGoodbyeGoodbyeHello.
+
+# we could write a regex to capture instances of the same two word characters in a row
+
+# (\w)\1
+# (start group 1
+# \w ; a word character
+# ) end group 1
+# \1 ; the contents of group 1
+
+
+# uppercase and lowercase are two different characters
+
+'''
+
+
+'''
+Instructions
+Write a regular expression to match cases of repeated words:
+We'll define a word as a series of one or more word characters preceded and followed by a boundary anchor.
+We'll define repeated words as the same word repeated twice, separated by a single whitespace character.
+'''
+
+pattern = r"\b(\w+)\s\1\b"
+repeated_words = titles[titles.str.contains(pattern)]
+
+#  Substituting Regular Expression Matches
+
+'''
+When we learned to work with basic string methods, we used the str.replace() method to replace simple
+ substrings. We can achieve the same with regular expressions using the re.sub() function. 
+ The basic syntax for re.sub() is:
+
+re.sub(pattern, repl, string, flags=0)
+
+The repl parameter is the text that you would like to substitute for the match. Let's look at a simple 
+example where we replace all capital letters in a string with dashes:
+
+string = "aBcDEfGHIj"
+​
+print(re.sub(r"[A-Z]", "-", string))
+
+a-c--f---j
+
+When working in pandas, we can use the Series.str.replace() method, which uses similar syntax:
+
+Series.str.replace(pat, repl, flags=0, regex=False)
+
+In pandas 2.0.1, it's important to note that when using regular expressions, you need to explicitly set 
+regex=True. Additionally, if you want to use the flags parameter, you must also set regex=True.
+
+Earlier, we discovered that there were multiple different capitalizations for SQL in our dataset. 
+Let's look at how we could make these uniform with the Series.str.replace() method and a regular expression:
+
+sql_variations = pd.Series(["SQL", "Sql", "sql"])
+
+sql_uniform = sql_variations.str.replace(r"sql", "SQL", flags=re.I, regex=True)
+print(sql_uniform)
+
+
+0    SQL
+1    SQL
+2    SQL
+dtype: object
+'''
+
+email_variations = pd.Series(['email', 'Email', 'e Mail',
+                        'e mail', 'E-mail', 'e-mail',
+                        'eMail', 'E-Mail', 'EMAIL'])
+
+
+'''
+Use a regular expression to replace each of the matches in email_variations with "email" and assign the result to email_uniform.Your regular expression should be compatible with the ignorecase flag, and should not match the word "emailing" or any other words that contain "email" as a substring.
+You may need to iterate several times when writing your regular expression in order to match every item.
+'''
+
+pattern = r'e[\s-]?mail'
+# \s is a whitespace character
+# [\s-] is a whitespace or a hyphen
+# ? is a zero or one occurence of the character before it, or we can write it as {0,1}
+
+email_uniform = email_variations.str.replace(pat=pattern, repl='email', flags=re.I, regex=True)
+
+
+'''
+Use a regular expression to replace all mentions of email in titles with "email". Assign the result to titles_clean.
+Note that passing the cases in email_variations does not guarantee passing all the cases in the titles column.
+'''
+
+titles_clean = titles.str.replace(pat=pattern, repl='email', flags=re.I, regex=True)
+
+
+# Extracting Domains from URLs
+
