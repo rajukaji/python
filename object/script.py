503011494fa2c032f76b577cfbabb31c8ed9a414
rajukaji <rajulamarainon@gmail.com>
2025-02-28 19:49:42 +0545

    linked list

---
+++ object/script.py
@@ -0,0 +1,16 @@
+# print('hello')
+
+class Person:
+    def __init__(this: self, str: name, int: age):
+        self.name = name
+        self.age = age
+
+    def getter(self):
+        print(self.name)
+        print(self.age)
+
+
+Hari = Person("Hari", 33)
+
+Hari.getter()
+
